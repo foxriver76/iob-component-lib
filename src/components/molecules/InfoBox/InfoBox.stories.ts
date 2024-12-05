82b7e310333338f667daccc748e78ac510375a10
Max Hauser <moritz.heusinger@gmail.com>
2024-12-05 15:16:47 +0100

    allow to make info text closeable (#18)
    * allow to make info text closeable
    
    * add docs

--- src/components/molecules/InfoBox/InfoBox.stories.ts
+++ src/components/molecules/InfoBox/InfoBox.stories.ts
@@ -1,5 +1,6 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import InfoBox from '@/components/molecules/InfoBox/InfoBox';
+import { fn } from '@storybook/test';
 
 type Story = StoryObj<typeof InfoBox>;
 
@@ -10,7 +11,9 @@ export default {
     args: {
         children:
             'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
-        type: 'info'
+        type: 'info',
+        closeable: false,
+        onClose: fn()
     },
     argTypes: {}
 } satisfies Meta<typeof InfoBox>;
@@ -26,5 +29,11 @@ export const Warning: Story = {
 export const Error: Story = {
     args: {
         type: 'error'
+    }
+};
+
+export const Closeable: Story = {
+    args: {
+        closeable: true
     }
 };
